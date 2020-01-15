1061d8e9bd8ba42b41c63c2b796c56a51db117ad
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:49:26 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var str:String ="10"
+   var num1 = 5
+   var num2:Int = str.toInt()
+   var toplam = num1+num2
+   print( "toplam  = $toplam")
+
+}
